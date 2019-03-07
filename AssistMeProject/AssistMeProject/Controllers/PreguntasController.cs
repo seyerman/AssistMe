0be1529b47827b0003dcbaaf645b07932eccf05b
Alejandro Barrera <alejobarre9@gmail.com>
2019-03-06 19:45:23 -0500

    Modulo preguntas - modelo

---
+++ AssistMeProject/AssistMeProject/Controllers/PreguntasController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AssistMeProject.Models;
+
+namespace AssistMeProject.Controllers
+{
+    public class PreguntasController : Controller
+    {
+        private readonly AssistMeProjectContext _context;
+
+        public PreguntasController(AssistMeProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Preguntas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pregunta.ToListAsync());
+        }
+
+        // GET: Preguntas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pregunta = await _context.Pregunta
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pregunta == null)
+            {
+                return NotFound();
+            }
+
+            return View(pregunta);
+        }
+
+        // GET: Preguntas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Preguntas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IsArchived,id,Title,Description,Date")] Pregunta pregunta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pregunta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pregunta);
+        }
+
+        // GET: Preguntas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pregunta = await _context.Pregunta.FindAsync(id);
+            if (pregunta == null)
+            {
+                return NotFound();
+            }
+            return View(pregunta);
+        }
+
+        // POST: Preguntas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IsArchived,id,Title,Description,Date")] Pregunta pregunta)
+        {
+            if (id != pregunta.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pregunta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PreguntaExists(pregunta.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pregunta);
+        }
+
+        // GET: Preguntas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pregunta = await _context.Pregunta
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pregunta == null)
+            {
+                return NotFound();
+            }
+
+            return View(pregunta);
+        }
+
+        // POST: Preguntas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pregunta = await _context.Pregunta.FindAsync(id);
+            _context.Pregunta.Remove(pregunta);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PreguntaExists(int id)
+        {
+            return _context.Pregunta.Any(e => e.id == id);
+        }
+    }
+}
---
+++ AssistMeProject/AssistMeProject/Models/Elemento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssistMeProject.Models
+{
+    public class Elemento : IComparable
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
