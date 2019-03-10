ecab75d8f8acdd660329399e427cc7c1162d5b7e
Javier Torres <javierandrestorresreyes@gmail.com>
2019-03-10 15:02:44 -0500

    Search classes

---
+++ AssistMeProject/AssistMeProject/Models/Searcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistMeProject.Models
+{
+    public class BM25Searcher
+    {
+        public List<SearchDocument> Documents { get; set; }
+
+        public List<ISearchable> Search(string query)
+        {
+            var punctuation = query.Where(Char.IsPunctuation).Distinct().ToArray();
+            var qTerms = query.Split().Select(x => x.Trim(punctuation));
+
+            return null;
+        }
+
+    }
+}
