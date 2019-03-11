6e20eae49c2627279eebc2d4b9d52ff6733fc059
Kliver Daniel Giron Castro <kliver.giron1@correo.icesi.edu.co>
2019-03-10 20:10:34 -0500

    Created classes User and AssistMe giving them some automatic properties to star work with

---
+++ AssistMeProject/AssistMeProject/Models/AssistMe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssistMeProject.Models
+{
+    public class AssistMe
+    {
+
+        private Dictionary<string, User> listUsers;
+
+        public AssistMe()
+        {
+
+            listUsers = new Dictionary<string, User>();
+
+        }
+
+        private void createTestUsers(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                string username = "Test"+i;
+                string email = i%2==0 ? username+".gaming@globant.com": username + ".consulting@globant.com";
+                User user = new User(i, email, "../data/photos/" + username, i%6,i%10,i%30,i%20,"I like to help people","Play guitar, play piano, food lover","Colombia","Medellin");
+                listUsers.Add(username,user);
+            }
+        }
+
+    }
+}
