9948571adf2307fa22e24914b1682b0e192ece2e
Kliver Daniel Giron Castro <kliver.giron1@correo.icesi.edu.co>
2019-03-11 15:44:08 -0500

    Merging with master manually

--- AssistMeProject/AssistMeProject/Startup.cs
+++ AssistMeProject/AssistMeProject/Startup.cs
@@ -35,14 +35,10 @@ namespace AssistMeProject
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            /*
+
             services.AddDbContext<AssistMeProjectContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("AssistMeProjectContext")));
-<<<<<<< HEAD
-                    */
-=======
 
->>>>>>> upstream/master
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
