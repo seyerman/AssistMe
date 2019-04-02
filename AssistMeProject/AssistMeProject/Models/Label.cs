a85804e71e4c607533327d5c3e5c74973bfcb667
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2019-04-02 17:18:25 -0500

    add popular tags component and top users component. changes in replies view with target of improving the appearance and functionality Delete icon field of class label

--- AssistMeProject/AssistMeProject/Models/Label.cs
+++ AssistMeProject/AssistMeProject/Models/Label.cs
@@ -7,12 +7,9 @@ namespace AssistMeProject.Models
 {
     public class Label
     {
-
         public int Id { get; set; }
 
         public string Tag { get; set; }
-
-        public string Icon { get; set; }
 
         public int NumberOfTimes { get; set; }
     }
