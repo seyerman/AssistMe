6cbd8a0e44cd5aa61172a874985f3a139e7220fa
Alejandro Barrera Lozano <alejobarre9@gmail.com>
2019-04-06 21:42:24 -0500

    Question_Tag class

--- AssistMeProject/AssistMeProject/Models/Label.cs
+++ AssistMeProject/AssistMeProject/Models/Label.cs
@@ -8,13 +8,8 @@ namespace AssistMeProject.Models
     public class Label
     {
         public int Id { get; set; }
-        public int QuestionId { get; set; }
         public string Tag { get; set; }
         public virtual Question Question { get; set; }
         public int NumberOfTimes { get; set; }
-        public Label()
-        {
-
-        }
     }
 }
---
+++ AssistMeProject/AssistMeProject/Models/Question_Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssistMeProject.Models
+{
+    public class Question_Tag
+    {
+        public int QuestionId { get; set; }
+        public int TagId { get; set; }
+    }
+}
