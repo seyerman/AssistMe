52e9c197bde81f1d4b3a5ad71e1c9a4586e2a332
lopez997 <42072817+lopez997@users.noreply.github.com>
2019-04-28 11:47:56 -0500

    .

--- AssistMeProject/AssistMeProject/Models/Question.cs
+++ AssistMeProject/AssistMeProject/Models/Question.cs
@@ -12,7 +12,7 @@ namespace AssistMeProject.Models
         [Required(ErrorMessage = "Agregue un Titulo a su pregunta"), MaxLength(150), Display(Name = "Titulo")]
         public string Title { get; set; }
         public virtual ICollection<Answer> Answers { get; set; }
-
+        [Display(Name = "Nombre")]
         public virtual List<QuestionLabel> QuestionLabels { get; set; }
 
         public bool AskAgain { get; set; }
