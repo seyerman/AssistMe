49da9587210ac965ec867640712f9f7ce75e4e15
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2019-05-22 18:15:46 -0500

    Agregados name space necesarios en user controller

--- AssistMeProject/AssistMeProject/Controllers/UsersController.cs
+++ AssistMeProject/AssistMeProject/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿using AssistMeProject.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
